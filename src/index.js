a90b24616f26836ee9483b317f6eef148ae34563
Kirill Mager <kiruha24@mail.ru>
2019-10-04 16:05:56 +0300

    feat: passed 2 tests from 22

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let count = 0;
+  let array = expression.split('*');
+  let arrayLength = array.length;
+
+  for (let index = 0; index < array.length; index++) {
+    if (array[index].length == 2) {
+      let number = Number(array[0]);
+      count = count + defaultFactorail (number);
+    } else if (array[index].length == 3) {
+      let number = Number(array[0]);
+      count = count + notDefaultFactorail (number);
+    } else {
+      
+    }
+  }
+
+  function defaultFactorail(number) {
+     return Math.trunc(number/5);
+  }
+  
+  function notDefaultFactorail(number) {
+    if (number % 2 == 0 ) {
+      return Math.trunc(number/10);
+    } else {
+      return 0;
+    }
+  }
+  return count;
 }
